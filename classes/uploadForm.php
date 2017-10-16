ac12b814baa8fbcd0d5e0f6b36f4f6c01f43642a
Dale <yl622@njit.edu>
2017-10-16 11:35:37 -0400

    Added a if statement to check if the user choose a file to upload

--- classes/uploadForm.php
+++ classes/uploadForm.php
@@ -13,27 +13,31 @@
             $target_file = $target_dir . basename($_FILES["uploadCSVFiles"]["name"]);
             $uploadOk = 1;
             $fileType = pathinfo($target_file,PATHINFO_EXTENSION);
-            if(isset($_POST["submit"])) {
-                $check = stringFunctions::stringCompare(strtolower($fileType),'csv');
-                if($check == 0) {
-                    $uploadOk = 1;
-                } else {
-                    $this->html .= htmlTags::changeRow('Sorry, file is not a CSV file');;
+            if ($_FILES["uploadCSVFiles"]["name"] != NULL) {
+                if(isset($_POST["submit"])) {
+                    $check = stringFunctions::stringCompare(strtolower($fileType),'csv');
+                    if($check == 0) {
+                        $uploadOk = 1;
+                    } else {
+                        $this->html .= htmlTags::changeRow('Sorry, file is not a CSV file');;
+                        $uploadOk = 0;
+                    }
+                }
+                if (file_exists($target_file)) {
+                    $this->html .= htmlTags::changeRow('Sorry, file already exists.');
                     $uploadOk = 0;
                 }
-            }
-            if (file_exists($target_file)) {
-               $this->html .= htmlTags::changeRow('Sorry, file already exists.');
-               $uploadOk = 0;
-            }
-            if ($uploadOk == 0) {
-                $this->html .= htmlTags::changeRow('Your file was not uploaded.');
+                if ($uploadOk == 0) {
+                    $this->html .= htmlTags::changeRow('Your file was not uploaded.');
+                } else {
+                    if (move_uploaded_file($_FILES["uploadCSVFiles"]["tmp_name"], $target_file)) {
+                        header('Location: index.php?page=tableDisplay&filename=' . $_FILES["uploadCSVFiles"]["name"]);
+                    } else {
+                        $this->html .= htmlTags::changeRow('Sorry, there was an error uploading your file.');
+                    }
+                }
             } else {
-               if (move_uploaded_file($_FILES["uploadCSVFiles"]["tmp_name"], $target_file)) {
-                   header('Location: index.php?page=tableDisplay&filename=' . $_FILES["uploadCSVFiles"]["name"]);
-               } else {
-                   $this->html .= htmlTags::changeRow('Sorry, there was an error uploading your file.');
-               }
+                $this->html .= htmlTags::changeRow('Please choose a file to upload');
             }
         }
     }

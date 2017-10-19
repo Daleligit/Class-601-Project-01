5e7d9ed08b7e86a17be20a3522893eb698c609e8
Dale <yl622@njit.edu>
2017-10-19 14:43:47 -0400

    Put unused htlmTags function into commit part

--- classes/htmlTags.php
+++ classes/htmlTags.php
@@ -44,9 +44,9 @@
          * to make sure the button can bring the user to the right page not based on their
          * view history.
         */
-        static public function backButton($input) {
-            return '<input type="button" onclick="javascript:history.back(-1);" value="' . $input . '">';
-        }
+        //static public function backButton($input) {
+        //    return '<input type="button" onclick="javascript:history.back(-1);" value="' . $input . '">';
+        //}
         static public function turnPage($pageName, $input) {
             return '<input type="button" onclick="location.href=\'' .$pageName . '\'" value="' . $input . '">';
        }
